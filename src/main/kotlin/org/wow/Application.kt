9c179e66129039d81de6f57000abf4ad123b3d81
Egalvi <egalvi@mail.ru>
2014-08-06 13:48:00 +0400

    and operation on Logic class implemented

--- src/main/kotlin/org/wow/Application.kt
+++ src/main/kotlin/org/wow/Application.kt
@@ -1,13 +1,15 @@
 package org.wow
 
 import com.epam.starwors.game.SocketGame
-import com.epam.starwors.galaxy.Move
-import com.epam.starwors.galaxy.Planet
+import com.epam.starwors.bot.Logic
 
 
 fun main(args : Array<String>) {
-    val game = SocketGame("10.20.60.2", 10040, "jat5s65p3lypamzg3ztzjyu2rt4tgqt1", { planets -> arrayListOf() })
+    val game = SocketGame("10.20.60.2", 10040, "jat5s65p3lypamzg3ztzjyu2rt4tgqt1", CustomLogic().and(CustomLogic()).and(CustomLogic()))
     game.start()
     print("Running...")
 
 }
+
+fun Logic.and(other: Logic): Logic =
+        Logic { planets-> this.step(planets)?.plus(other.step(planets)!!)?.toArrayList() }
