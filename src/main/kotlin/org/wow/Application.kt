173353430e97cd5226e6a33d1b8ee8e1a0c83e38
Egalvi <egalvi@mail.ru>
2014-08-06 13:13:21 +0400

    Application made runnable

--- src/main/kotlin/org/wow/Application.kt
+++ src/main/kotlin/org/wow/Application.kt
@@ -1,10 +1,13 @@
 package org.wow
 
 import com.epam.starwors.game.SocketGame
+import com.epam.starwors.galaxy.Move
+import com.epam.starwors.galaxy.Planet
 
 
 fun main(args : Array<String>) {
-    SocketGame("10.0.2.3", 8080, "token", {planets -> null})
+    val game = SocketGame("10.20.60.2", 10040, "jat5s65p3lypamzg3ztzjyu2rt4tgqt1", { planets -> arrayListOf() })
+    game.start()
     print("Running...")
 
 }
