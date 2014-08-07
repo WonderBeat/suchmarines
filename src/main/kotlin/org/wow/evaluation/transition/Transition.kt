3088036ab61f74b9166b8aee5be96369ab7bfe26
Egalvi <egalvi@mail.ru>
2014-08-07 17:47:08 +0400

    Implemented functionality for finding best transition for each step of the game

---
+++ src/main/kotlin/org/wow/evaluation/transition/Transition.kt
@@ -0,0 +1,9 @@
+package org.wow.evaluation.transition
+
+import org.wow.logger.World
+
+/**
+ *
+ */
+public class Transition(val sourceWorld: World, val resultWorld: World, val playerName: String, val playersPowerDifference: Double) {
+}
