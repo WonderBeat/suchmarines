ed89e5cb941784368c26c4186a5b2d75b8b23983
OctaDecimal <borov.htid@gmail.com>
2014-08-08 14:53:06 +0400

    Planet size category

--- src/main/kotlin/org/wow/learning/vectorizers/planet/NeighborBasedPlanetVectorizer.kt
+++ src/main/kotlin/org/wow/learning/vectorizers/planet/NeighborBasedPlanetVectorizer.kt
@@ -8,6 +8,7 @@ import com.epam.starwors.galaxy.Planet
 import org.wow.learning.enemiesAround
 import org.wow.learning.planetPower
 import org.wow.learning.friendsAround
+import org.apache.mahout.vectorizer.encoders.ConstantValueEncoder
 
 
 public data class PlanetTransition(val transition: org.wow.learning.vectorizers.Transition, val planet: Planet)
@@ -17,12 +18,14 @@ public class NeighborBasedPlanetVectorizer : Vectorizer<PlanetTransition, Vector
     val enemiesAround = ContinuousValueEncoder("enemies")
     val friendsAround = ContinuousValueEncoder("friend")
     val planetPower = ContinuousValueEncoder("power")
+    var planetSize = ConstantValueEncoder("size")
 
     override fun vectorize(input: PlanetTransition): Vector {
         val vector = RandomAccessSparseVector()
         enemiesAround.addToVector(null: ByteArray?, input.planet.enemiesAround(input.transition.from).toDouble(), vector)
         friendsAround.addToVector(null: ByteArray?, input.planet.friendsAround(input.transition.from).toDouble(), vector)
         planetPower.addToVector(null: ByteArray?, input.planet.planetPower(input.transition.from).toDouble(), vector)
+        planetSize.addToVector(null: ByteArray?, input.planet.getType()!!.getLimit().toDouble(), vector)
         return vector
     }
 }
