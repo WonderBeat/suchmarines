bf402765de5da417a0b201eab24ce02b7beade2e
WonderBeat <borov.htid@gmail.com>
2014-08-10 19:40:28 +0400

    refactored. function extracted

--- src/main/kotlin/org/wow/evaluation/transition/BestTransitionsFinder.kt
+++ src/main/kotlin/org/wow/evaluation/transition/BestTransitionsFinder.kt
@@ -6,7 +6,7 @@ import org.wow.evaluation.Evaluator
 
 public class BestTransitionsFinder(val evaluator: Evaluator) {
     fun findBestTransitions(game: List<World>): Collection<Transition> {
-        val players = game.first().planets!!.map { it.getOwner() }.filterNot { it!!.isEmpty() }.distinct()
+        val players = listPlayersOnMap(game)
         val transitions = pairs(game).map { pair ->
             players.map { player ->
                 Transition(pair.first, pair.second, player!!)
@@ -14,6 +14,10 @@ public class BestTransitionsFinder(val evaluator: Evaluator) {
         }
         return transitions.map {
             it.maxBy { evaluator.difference(it.playerName, it.sourceWorld, it.resultWorld) }!! }
+    }
+
+    fun listPlayersOnMap(game: List<World>): Set<String?> {
+        return game.first().planets!!.map { it.getOwner() }.filterNot { it!!.isEmpty() }.distinct()
     }
 
     /**
