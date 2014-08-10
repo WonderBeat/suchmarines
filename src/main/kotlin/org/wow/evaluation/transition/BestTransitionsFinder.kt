9e00731f2a5324760eb381361838ae52f661238f
WonderBeat <borov.htid@gmail.com>
2014-08-10 19:37:12 +0400

    World pairs creator refactored

--- src/main/kotlin/org/wow/evaluation/transition/BestTransitionsFinder.kt
+++ src/main/kotlin/org/wow/evaluation/transition/BestTransitionsFinder.kt
@@ -5,7 +5,7 @@ import org.wow.evaluation.Evaluator
 
 
 public class BestTransitionsFinder(val evaluator: Evaluator) {
-    fun findBestTransitions(game: Collection<World>): Collection<Transition> {
+    fun findBestTransitions(game: List<World>): Collection<Transition> {
         val players = game.first().planets!!.map { it.getOwner() }.filterNot { it!!.isEmpty() }.distinct()
         val transitions = pairs(game).map { pair ->
             players.map { player ->
@@ -16,5 +16,9 @@ public class BestTransitionsFinder(val evaluator: Evaluator) {
             it.maxBy { evaluator.difference(it.playerName, it.sourceWorld, it.resultWorld) }!! }
     }
 
-    private fun pairs(game: Collection<World>): List<Pair<World, World>> = game.take(game.size - 1).zip(game.drop(1))
+    /**
+     * Creates World pairs
+     * (first, second), (second, third) ... (n-1, n)
+     */
+    private fun pairs(game: List<World>): List<Pair<World, World>> = game.take(game.size - 1).zip(game.tail)
 }
