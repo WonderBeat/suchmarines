6fcc93c864fe5ffd05173f318aa14bd9a3353b3b
WonderBeat <borov.htid@gmail.com>
2014-08-13 12:22:36 +0400

    Adjust coefficients

--- src/main/kotlin/org/wow/evaluation/UserPowerEvaluator.kt
+++ src/main/kotlin/org/wow/evaluation/UserPowerEvaluator.kt
@@ -9,8 +9,8 @@ import org.wow.logger.World
  */
 public class UserPowerEvaluator : Evaluator {
 
-    private val unitsCoefficient = 0.2
-    private val planetsCoefficient = 0.4
+    private val unitsCoefficient = 0.5
+    private val planetsCoefficient = 0.2
     private val regenerationRateCoefficient = 0.3
 
     override fun evaluate(playerName: String, world: World): Double {
