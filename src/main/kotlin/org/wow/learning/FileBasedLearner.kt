8c7ebae41912d4c96c43e5f17102de238f1d24a7
OctaDecimal <borov.htid@gmail.com>
2014-08-13 14:34:32 +0400

    Reduce replaced with compose. It's just easier to read

--- src/main/kotlin/org/wow/learning/FileBasedLearner.kt
+++ src/main/kotlin/org/wow/learning/FileBasedLearner.kt
@@ -8,25 +8,29 @@ import reactor.core.composable.spec.Streams
 import org.wow.logger.LogsParser
 import reactor.core.Environment
 import reactor.function.Consumer
+import java.util.concurrent.TimeUnit
 
 public class FileBasedLearner<T>(
                           val env: reactor.core.Environment,
                           val files: List<File>,
                           val parser: LogsParser,
-                          val vectorizer: (List<World>) -> List<T>) {
+                          val vectorizer: (List<World>) -> List<T>,
+                          val timeoutSec: Long = 200) {
 
     private val logger = LoggerFactory.getLogger(javaClass<FileBasedLearner<T>>())!!
 
-    fun <B, K : Learner<T, B>> learn(data: K): K {
+    fun <B, K : Learner<T, B>> learn(learner: K): K {
         val doneDefer = Promises.defer<K>()!!.env(env)!!.get()!!
-        val completePromise = doneDefer.compose()
+        val completePromise = doneDefer.compose()!!
         val complete = Consumer<K> { doneDefer.accept(it) }
-        val deferred = Streams.defer<File>()!!.env(env)!!.dispatcher(Environment.RING_BUFFER)!!.get();
-        deferred!!.compose()!!.map { file -> parser.parse(file!!) }!!.map { game -> vectorizer(game!!) }!!
-                .reduce ({ (current) -> current!!.getT1()!!.forEach { current.getT2()!!.learn(it) }; current.getT2() },
-                        data)!!
+        val deferred = Streams.defer<File>()!!.env(env)!!.dispatcher(Environment.THREAD_POOL)!!.get();
+        deferred!!.compose()!!
+                .map { file -> parser.parse(file!!) }!!.map { game -> vectorizer(game!!) }!!
+                .batch(files.size)!!
+                .collect()!!
+                .map { it!!.flatMap{ it }.forEach { learner.learn(it) }; learner}!!
                 .consume(complete)
         files.forEach{ deferred.accept(it) }
-        return completePromise!!.await()!!
+        return completePromise.await(timeoutSec, TimeUnit.SECONDS)!!
     }
 }
