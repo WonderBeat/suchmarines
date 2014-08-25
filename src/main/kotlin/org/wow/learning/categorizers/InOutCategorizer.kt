7bf3968890aef516c3af36ef2e6dabb6a17e202a
WonderBeat <borov.htid@gmail.com>
2014-08-25 11:15:37 +0400

    Support movies could be extracted out of all moves for current planet

--- src/main/kotlin/org/wow/learning/categorizers/InOutCategorizer.kt
+++ src/main/kotlin/org/wow/learning/categorizers/InOutCategorizer.kt
@@ -2,19 +2,26 @@ package org.wow.learning.categorizers
 
 import org.wow.learning.vectorizers.planet.PlanetTransition
 import com.epam.starwors.galaxy.Planet
+import org.wow.learning.enemiesNeighbours
+import org.wow.learning.friendsNeighbours
 
-private fun Planet.percentFromPlanetUnits(units: Int): Int = (units * 100.0 / this.getUnits()).toInt()
+private fun Planet.percentFromPlanetUnits(units: Int): Int = (units * 100.0 / this.getUnits().toDouble()).toInt()
 
 fun inOutCategorizer(transition: PlanetTransition): Int {
     val outMoves = transition.moves.filter { it.from == transition.from.getId() }
     val inMoves = transition.moves.filter { it.to == transition.from.getId() }
+    val inSupportMoves = inMoves.filter { move ->
+        transition.from.getNeighbours()!!
+                .filter { it.getId() == move.from }
+                .all { it.getOwner() == transition.from.getOwner() }
+    }
     val outUnits = outMoves.fold(0, { (acc, move) -> acc + move.unitCount })
-    val inUnits = inMoves.fold(0, { (acc, move) -> acc + move.unitCount })
+    val inSupportUnits = inSupportMoves.fold(0, { (acc, move) -> acc + move.unitCount })
     val planetWasCaptured = transition.from.getOwner() != transition.to.getOwner()
     return when {
         planetWasCaptured -> InOutMove(`in`= 100).toInt()
         outUnits > 0 -> InOutMove(out = transition.from.percentFromPlanetUnits(outUnits)).toInt()
-        inUnits > 0 -> InOutMove(`in` = transition.from.percentFromPlanetUnits(inUnits)).toInt()
+        inSupportUnits > 0 -> InOutMove(`in` = transition.from.percentFromPlanetUnits(inSupportUnits)).toInt()
         else -> InOutMove().toInt()
     }
 }
